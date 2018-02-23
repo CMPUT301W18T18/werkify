057f8f74e73dc73735c858bcb757ec1cb8325ef5
StefanVidakovic <vidakovi@ualberta.ca>
2018-02-23 14:02:58 -0700

    Added Task skeleton

---
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/ConcreteTask.java
@@ -0,0 +1,115 @@
+/* Copyright 2018 CMPUT301W18T18
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.ualberta.cs.wrkify;
+
+/**
+*
+*/
+
+import java.awt.Image;
+import java.util.ArrayList;
+
+import javax.tools.DocumentationTool.Location;
+
+public class ConcreteTask {
+    private String title;
+    private String description;
+    private ArrayList<Image> imageList;
+    private Location location;
+    private CheckList checkList;
+    private ArrayList<Bid> bidList;
+    private ConcreteUser requester;
+    private ConcreteUser provider;
+    private TaskStatus status;
+    private Double price;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public ArrayList<Image> getImageList() {
+        return imageList;
+    }
+    public void setImageList(ArrayList<Image> imageList) {
+        this.imageList = imageList;
+    }
+    public Location getLocation() {
+        return location;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public CheckList getCheckList() {
+        return checkList;
+    }
+    public void setCheckList(CheckList checkList) {
+        this.checkList = checkList;
+    }
+    public ArrayList<Bid> getBidList() {
+        return bidList;
+    }
+    public void setBidList(ArrayList<Bid> bidList) {
+        this.bidList = bidList;
+    }
+    public ConcreteUser getRequester() {
+        return requester;
+    }
+    public void setRequester(ConcreteUser requester) {
+        this.requester = requester;
+    }
+    public ConcreteUser getProvider() {
+        return provider;
+    }
+    public void setProvider(ConcreteUser provider) {
+        this.provider = provider;
+    }
+    public TaskStatus getStatus() {
+        return status;
+    }
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Bid getLowestBid() {
+        sortBidList();
+        return this.bidList.get(0);
+    }
+    public void addBid(Bid bid) {
+        bidList.add(bid);
+        sortBidList();
+    }
+    public void sortBidList() {
+        this.bidList.sort(new BidComparator());
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void acceptBid(Bid bid) {
+        setPrice(bid.getValue());
+    }
+}
