ebfabd7269479cb1dd9ef68dc87387b16263edee
Peter Elliott <p.elliott332@gmail.com>
2018-03-18 17:48:59 -0600

    added saving and loading of user

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/Session.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/Session.java
@@ -17,36 +17,54 @@
 
 package ca.ualberta.cs.wrkify;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by peter on 18/03/18.
  */
 
 public class Session {
+    private static final String FILENAME = "ca.ualberta.cs.wrkify.Session";
     private static Session instance;
 
     private User user;
 
     private Session() {}
 
-    public static Session getInstance() {
+    public static Session getInstance(Context context, RemoteClient client) {
         if (instance == null) {
             instance = new Session();
         }
 
         if (instance.user == null) {
-            instance.load();
+            instance.load(context, client);
         }
 
         return instance;
+    }
+
+    public static Session getInstance(Context context) {
+        return getInstance(context, WrkifyClient.getInstance());
     }
 
     public User getUser() {
         return this.user;
     }
 
-    public void setUser(User user) {
+    public void setUser(User user, Context context) {
         this.user = user;
-        save();
+        save(context);
     }
 
     public void logout() {
@@ -54,11 +72,40 @@ public class Session {
         //TODO clear savefile;
     }
 
-    private void save() {
-        //TODO save the id
+    private void save(Context context) {
+        // this is adapted from the lab 26-01-2018
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(this.user.getId(), out);
+
+            out.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
-    private void load() {
-        //TODO reload the user from id
+    private void load(Context context, RemoteClient client) {
+        // this is adapted from the lab 26-01-2018
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+            String id = gson.fromJson(in, String.class);
+
+            this.user = client.download(id, User.class);
+        } catch (FileNotFoundException e) {
+            this.user = null;
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
