bc462d2e584a03b8258c0728780aedbd24ccb51b
Peter Elliott <p.elliott332@gmail.com>
2018-03-19 00:34:20 -0600

    added bidding to server

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/ViewTaskBottomSheetFragment.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/ViewTaskBottomSheetFragment.java
@@ -17,7 +17,7 @@
 
 package ca.ualberta.cs.wrkify;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
