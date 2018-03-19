876c9dd3fc70dc22ad5b3fe6ca0fbf71f58502d7
abxjdorn <abxjdorn@users.noreply.github.com>
2018-03-18 23:23:30 -0600

    TaskList integration

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/TaskListFragment.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/TaskListFragment.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -80,8 +81,10 @@ public class TaskListFragment extends Fragment {
             view.findViewById(R.id.taskListEmptyMessage).setVisibility(View.VISIBLE);
         }
         else {
+            TaskListAdapter<Task> taskListAdapter = new TaskListAdapter<Task>(getContext(), tasks);
             RecyclerView recyclerView = view.findViewById(R.id.taskListView);
-            recyclerView.setAdapter(null); // TODO adapter doesn't exist yet
+            recyclerView.setAdapter(taskListAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         }
 
         return view;
