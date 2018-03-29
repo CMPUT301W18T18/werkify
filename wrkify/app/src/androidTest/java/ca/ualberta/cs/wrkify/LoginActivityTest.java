d8d40683eba84ed74e83bd41124b20024ddc09fa
abxjdorn <abxjdorn@users.noreply.github.com>
2018-03-29 16:51:16 -0600

    trying intent tests...

--- wrkify/app/src/androidTest/java/ca/ualberta/cs/wrkify/LoginActivityTest.java
+++ wrkify/app/src/androidTest/java/ca/ualberta/cs/wrkify/LoginActivityTest.java
@@ -18,25 +18,61 @@
 package ca.ualberta.cs.wrkify;
 
 
+import android.content.ComponentName;
 import android.content.Intent;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.intent.Intents.*;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
- * Tests for LoginActivity (and RegisterActivity).
+ * Tests for LoginActivity.
  */
+@RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
     @Rule
-    public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule<>(
-            LoginActivity.class, false, false);
+    public IntentsTestRule<LoginActivity> intentsTestRule =
+            new IntentsTestRule<>(LoginActivity.class);
+
+    @Before
+    public void setupClient() {
+        WrkifyClient.setRemoteClient(new MockRemoteClient());
+        WrkifyClient.getInstance().create(User.class, "user4", "user4@example.com", "5018293749");
+    }
 
     /**
-     * Launches LoginActivity
+     * Log in as a user that exists.
      */
     @Test
     public void testLoginActivity() {
-        activityTestRule.launchActivity(new Intent());
+        onView(withId(R.id.loginField)).perform(typeText("user4"));
+        onView(withId(R.id.loginButton)).perform(click());
+
+        assertEquals("user4", Session.getInstance(intentsTestRule.getActivity()).getUser().getUsername());
+        intended(hasComponent(new ComponentName(intentsTestRule.getActivity(), MainActivity.class)));
+    }
+
+    /**
+     * Login as a user that does not exist
+     */
+    @Test
+    public void testLoginFail() {
+        onView(withId(R.id.loginField)).perform(typeText("user5"));
+        onView(withId(R.id.loginButton)).perform(click());
+
+        
     }
 }
