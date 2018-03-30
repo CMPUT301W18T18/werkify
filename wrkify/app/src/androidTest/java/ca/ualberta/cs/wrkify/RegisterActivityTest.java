52f4873ddb5ecd2e0f43bb0dfa0a25c299e17b87
abxjdorn <abxjdorn@users.noreply.github.com>
2018-03-30 14:52:59 -0600

    RegisterActivity intent tests

--- wrkify/app/src/androidTest/java/ca/ualberta/cs/wrkify/RegisterActivityTest.java
+++ wrkify/app/src/androidTest/java/ca/ualberta/cs/wrkify/RegisterActivityTest.java
@@ -17,18 +17,31 @@
 
 package ca.ualberta.cs.wrkify;
 
+import android.app.Activity;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Tests for RegisterActivity.
  */
 @RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest extends AbstractIntentTest<RegisterActivity> {
+    private User existingUser;
+
     @Override
     protected Class<RegisterActivity> getActivityClass() {
         return RegisterActivity.class;
@@ -36,6 +49,109 @@ public class RegisterActivityTest extends AbstractIntentTest<RegisterActivity> {
 
     @Override
     protected void createMockData() {
-        
+        this.existingUser = WrkifyClient.getInstance().create(User.class, "existingUser", "a@a", "1840392743");
+    }
+
+    /**
+     * Utility method to enter user information and press the Register button.
+     * @param username Username to enter
+     * @param email Email to enter
+     * @param phoneNumber Phone number to enter
+     */
+    private void registerWith(String username, String email, String phoneNumber) {
+        onView(withId(R.id.registerField)).perform(typeText(username));
+        onView(withId(R.id.registerEmail)).perform(typeText(email));
+        onView(withId(R.id.registerPhoneNumber)).perform(typeText(phoneNumber));
+        onView(withId(R.id.registerButton)).perform(click());
+    }
+
+    /**
+     * Utility method to get the session user.
+     * @return Session user
+     */
+    private User getSessionUser() {
+        return Session.getInstance(intentsTestRule.getActivity()).getUser();
+    }
+
+    /**
+     * Register a new user.
+     * Should: create new user
+     *         finish activity with RESULT_OK
+     */
+    @Test
+    public void testRegister() {
+        client.mockNextSearch();
+
+        registerWith("newUser", "new-user@example.com", "1938059920");
+
+        User sessionUser = getSessionUser();
+        assertEquals("newUser", sessionUser.getUsername());
+        assertEquals("new-user@example.com", sessionUser.getEmail());
+        assertEquals("1938059920", sessionUser.getPhoneNumber());
+
+        assertTrue(intentsTestRule.getActivity().isFinishing());
+        assertEquals(Activity.RESULT_OK, intentsTestRule.getActivityResult().getResultCode());
+    }
+
+    /**
+     * Register with empty username.
+     * Should: fail
+     */
+    @Test
+    public void testEmptyUsername() {
+        registerWith("", "new-user@example.com", "1938059920");
+
+        assertNull(getSessionUser());
+        assertFalse(intentsTestRule.getActivity().isFinishing());
+    }
+
+    /**
+     * Register with empty email.
+     * Should: fail
+     */
+    @Test
+    public void testEmptyEmail() {
+        registerWith("newUser", "", "1938059920");
+
+        assertNull(getSessionUser());
+        assertFalse(intentsTestRule.getActivity().isFinishing());
+    }
+
+    /**
+     * Register with empty phone number.
+     * Should: fail
+     */
+    @Test
+    public void testEmptyPhoneNumber() {
+        registerWith("newUser", "new-user@example.com", "");
+
+        assertNull(getSessionUser());
+        assertFalse(intentsTestRule.getActivity().isFinishing());
+    }
+
+    /**
+     * Register with invalid email.
+     * Should: fail
+     */
+    @Test
+    public void testInvalidEmail() {
+        registerWith("newUser", "not a valid email", "1938059920");
+
+        assertNull(getSessionUser());
+        assertFalse(intentsTestRule.getActivity().isFinishing());
+    }
+
+    /**
+     * Register with an already-registered username.
+     * Should: fail
+     */
+    @Test
+    public void testUnavailableUsername() {
+        client.mockNextSearch(existingUser);
+
+        registerWith("existingUser", "new-user@example.com", "1938059920");
+
+        assertNull(getSessionUser());
+        assertFalse(intentsTestRule.getActivity().isFinishing());
     }
 }
