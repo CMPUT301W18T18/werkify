8b6bf42c5cfaaad3273f2dcdf04246e755d21b27
Peter Elliott <pelliott@ualberta.ca>
2018-04-02 11:54:28 -0600

    added snackbar notification to login activity

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/LoginActivity.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/LoginActivity.java
@@ -20,6 +20,7 @@ package ca.ualberta.cs.wrkify;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
@@ -112,9 +113,17 @@ public class LoginActivity extends AppCompatActivity {
                 session.setUser(user, this);
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
+            } else {
+                Snackbar snack = Snackbar.make(findViewById(android.R.id.content),
+                        R.string.login_fail, Snackbar.LENGTH_LONG);
+                snack.setAction("Action", null);
+                snack.show();
             }
         } catch (IOException e) {
-
+            Snackbar snack = Snackbar.make(findViewById(android.R.id.content),
+                    R.string.no_internet, Snackbar.LENGTH_LONG);
+            snack.setAction("Action", null);
+            snack.show();
         }
     }
 
