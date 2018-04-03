ceeacd5a6f4228b0917dabd2ee7b43fb3ff9cd72
Taylor Folkersen <folkerse@ualberta.ca>
2018-04-03 17:22:38 -0600

    Very basic prototype in TestActivity

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/TestActivity.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/TestActivity.java
@@ -17,18 +17,33 @@
 
 package ca.ualberta.cs.wrkify;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class TestActivity extends AppCompatActivity {
+    protected static final int REQUEST_IMAGE_CAMERA = 1;
+    protected static final int REQUEST_IMAGE_GALLERY = 2;
+
+
+    protected ImageView thumbnailView;
+    protected ImageView fullImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        thumbnailView = findViewById(R.id.testImageThumbnail);
+        fullImageView = findViewById(R.id.testImageFull);
 
         Button testButton = findViewById(R.id.testButton);
         testButton.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +54,52 @@ public class TestActivity extends AppCompatActivity {
         });
     }
 
+    protected void setImages(Bitmap thumbnail, Bitmap fullImage) {
+        thumbnailView.setImageBitmap(thumbnail);
+        fullImageView.setImageBitmap(fullImage);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAMERA && resultCode == RESULT_OK) {
+            Bundle b = data.getExtras();
+            Bitmap thumbnail = (Bitmap) b.get("data");
+            setImages(thumbnail, thumbnail);
+        }
+    }
+
+    protected void openCamera() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (i.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(i, REQUEST_IMAGE_CAMERA);
+        }
+    }
+
+    protected void openGallery() {
+
+    }
+
     protected void buttonClicked() {
-        Log.i("*****", "Clicked the button");
+        //Show the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.get_image_dialog_title).setItems(R.array.get_image_dialog_choices, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        openCamera();
+                        break;
+
+                    case 1:
+                        openGallery();
+                        break;
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 }
