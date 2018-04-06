f20ca33bc783fe4726a3ab5fe36e98dfb94761fa
Peter Elliott <pelliott@ualberta.ca>
2018-04-06 16:41:13 -0600

    made RemoteTaskList an implementation of AbstractList

--- wrkify/app/src/main/java/ca/ualberta/cs/wrkify/RemoteTaskList.java
+++ wrkify/app/src/main/java/ca/ualberta/cs/wrkify/RemoteTaskList.java
@@ -18,11 +18,10 @@
 package ca.ualberta.cs.wrkify;
 
 import java.io.IOException;
+import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
 
-public class RemoteTaskList implements List<Task> {
+public class RemoteTaskList extends AbstractList<Task> {
     private RemoteClient client;
     private ArrayList<RemoteReference<Task>> tasks;
 
@@ -37,11 +36,6 @@ public class RemoteTaskList implements List<Task> {
     }
 
     @Override
-    public void add(int index, Task element) {
-        this.tasks.add(index, element.<Task>reference());
-    }
-
-    @Override
     public Task get(int index) {
         try {
             return this.tasks.get(index).getRemote(this.client, Task.class);
@@ -53,93 +47,7 @@ public class RemoteTaskList implements List<Task> {
     }
 
     @Override
-    public Task set(int index, Task element) {
-        try {
-            return this.tasks.set(index, element.<Task>reference())
-                    .getRemote(this.client, Task.class);
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    @Override
-    public Task remove(int index) {
-        try {
-            return this.tasks.remove(index)
-                    .getRemote(this.client, Task.class);
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    @Override
-    public ListIterator<Task> listIterator() {
-        return this.new Iterator();
-    }
-
-    @Override
-    public ListIterator<Task> listIterator(int index) {
-        return this.new Iterator(index);
-    }
-
-    private class Iterator implements ListIterator<Task> {
-        private int index;
-
-        public Iterator() {
-            this.index = 0;
-        }
-
-        public Iterator(int index) {
-            this.index = index;
-        }
-
-        @Override
-        public boolean hasNext() {
-            // note that task is a member of RemoteTaskList
-            return index < tasks.size();
-        }
-
-        @Override
-        public boolean hasPrevious() {
-            return index > 0;
-        }
-
-        @Override
-        public Task next() {
-            Task task = get(this.index);
-            this.index++;
-            return task;
-        }
-
-        @Override
-        public int nextIndex() {
-            return this.index;
-        }
-
-        @Override
-        public Task previous() {
-            this.index--;
-            return get(this.index);
-        }
-
-        @Override
-        public int previousIndex() {
-            return this.index - 1;
-        }
-
-        @Override
-        public void add(Task task) {
-            throw new UnsupportedOperationException("Iterator does not support add()");
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException("Iterator does not support remove()");
-        }
-
-        @Override
-        public void set(Task task) {
-            throw new UnsupportedOperationException("Iterator does not support set()");
-        }
+    public int size() {
+        return this.tasks.size();
     }
 }
